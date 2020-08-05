3722d1100dad5beb8d2ca90209c9a573df2fde91
Insanefoam <spartaq123456789@gmail.com>
2020-08-05 11:38:34 +0600

    add basics components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Registration from "./pages/Registration";
 
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/registration">
+          <Registration />
+        </Route>
+      </Switch>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/pages/Registration/Registration.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../../components/Header";
+
+const Registration = () => {
+  return (
+    <div>
+      <Header></Header>
+      <div>Registration page</div>
+    </div>
+  );
+};
+
+export default Registration;
