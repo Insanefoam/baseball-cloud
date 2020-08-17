70ead2c317bf3898c52eb650a8abfa14c81f29e3
Insanefoam <spartaq123456789@gmail.com>
2020-08-17 14:25:25 +0600

    configure signed profile aside

--- src/routes/Profile/Profile.js
+++ src/routes/Profile/Profile.js
@@ -2,15 +2,29 @@ import React, { useState, useEffect } from "react";
 import "./Profile.css";
 import UnsignedProfile from "./components/UnsignedProfile/UnsignedProfile";
 import { getCurrentProfile } from "api";
+import SignedProfile from "./components/SignedProfile";
+import { useLocation } from "react-router-dom";
+
+const idRegex = /\d+/;
 
 const Profile = () => {
+  const location = useLocation();
+  const path = location.pathname;
+  const id = path.match(idRegex) ? path.match(idRegex)[0] : undefined;
   const [profileInfo, setProfileInfo] = useState({});
   useEffect(() => {
     getCurrentProfile().then((res) =>
       setProfileInfo(res.data.data.current_profile)
     );
   }, []);
-  return profileInfo.first_name ? <div>Signed</div> : <UnsignedProfile />;
+
+  return id ? (
+    <SignedProfile id={id} />
+  ) : profileInfo.first_name ? (
+    <SignedProfile id={profileInfo.id} />
+  ) : (
+    <UnsignedProfile />
+  );
 };
 
 export default Profile;
--- src/routes/Profile/components/SignedProfile/SignedProfile.js
+++ src/routes/Profile/components/SignedProfile/SignedProfile.js
@@ -1,22 +1,108 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "components/Header";
 import Footer from "components/Footer";
 import "./SignedProfile.css";
 import Navigation from "components/Navigation";
 import Arrow from "components/Arrow";
-import UploadAvatarForm from "components/UploadAvatarForm";
-import ProfileInfoForm from "components/ProfileInfoForm";
+import { getProfile } from "api";
+import AgeSVG from "images/AgeSVG";
+import HeightSVG from "images/HeightSVG";
+import WeightSVG from "images/WeightSVG";
+import ThrowsSVG from "images/ThrowsSVG";
+import BatsSVG from "images/BatsSVG";
 
-const SignedProfile = () => {
+const capitalize = (str) => {
+  return str ? str.charAt(0).toUpperCase() + str.slice(1) : undefined;
+};
+
+const SignedProfile = ({ id }) => {
+  const [profileInfo, setProfileInfo] = useState({});
+  useEffect(() => {
+    getProfile(id).then((res) => setProfileInfo(res.data.data.profile));
+  }, [id]);
+  console.log(profileInfo);
   return (
     <div className="container">
       <Header navigation={Navigation} />
       <section className="profile">
         <aside className="profile__aside">
-          <div className="profile__aside-avatar">
-            <UploadAvatarForm />
+          <div className="userinfo">
+            <div className="userinfo__avatar">
+              <button>Favorite</button>
+              <div></div>
+            </div>
+            <div className="userinfo__bio">
+              <div className="userinfo__bio-big">{`${profileInfo.first_name} ${profileInfo.last_name}`}</div>
+              <div>{capitalize(profileInfo.position)}</div>
+            </div>
           </div>
-          <ProfileInfoForm></ProfileInfoForm>
+          <div className="personalinfo">
+            <div className="personalinfo__item">
+              <div className="personalinfo__item-left">
+                <AgeSVG></AgeSVG>
+                Age
+              </div>
+              <div className="personalinfo__item-right">{profileInfo.age}</div>
+            </div>
+            <div className="personalinfo__item">
+              <div className="personalinfo__item-left">
+                <HeightSVG></HeightSVG>
+                Height
+              </div>
+              <div className="personalinfo__item-right">{`${profileInfo.feet} ft ${profileInfo.inches} in`}</div>
+            </div>
+            <div className="personalinfo__item">
+              <div className="personalinfo__item-left">
+                <WeightSVG></WeightSVG>
+                Weight
+              </div>
+              <div className="personalinfo__item-right">{`${profileInfo.weight} lbs`}</div>
+            </div>
+            <div className="personalinfo__item">
+              <div className="personalinfo__item-left">
+                <ThrowsSVG></ThrowsSVG>
+                Throws
+              </div>
+              <div className="personalinfo__item-right">
+                {capitalize(profileInfo.throws_hand)}
+              </div>
+            </div>
+            <div className="personalinfo__item">
+              <div className="personalinfo__item-left">
+                <BatsSVG></BatsSVG>
+                Bats
+              </div>
+              <div className="personalinfo__item-right">
+                {capitalize(profileInfo.bats_hand)}
+              </div>
+            </div>
+          </div>
+          <div className="schoolinfo">
+            <div className="schoolinfo__item">
+              <h1>School</h1>
+              <h2>
+                {profileInfo.school ? profileInfo.school.name : undefined}
+              </h2>
+            </div>
+            <div className="schoolinfo__item">
+              <h1>School Year</h1>
+              <h2>{capitalize(profileInfo.school_year)}</h2>
+            </div>
+            <div className="schoolinfo__item">
+              <h1>Team</h1>
+              <h2>
+                {profileInfo.teams ? profileInfo.teams[0].name : undefined}
+              </h2>
+            </div>
+            <div className="schoolinfo__item">
+              <h1>Facility</h1>
+              <h2>
+                {profileInfo.facilities
+                  ? profileInfo.facilities[0].u_name
+                  : undefined}
+              </h2>
+            </div>
+          </div>
         </aside>
         <main className="profile__main">
           <div className="prhelp">
