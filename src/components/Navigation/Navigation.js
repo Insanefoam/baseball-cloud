52956a7a6a9ce6f45f5203090b2ea3d3aa620b84
Insanefoam <spartaq123456789@gmail.com>
2020-08-19 12:01:58 +0600

    configure dropdown menu on navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./Navigation.css";
 import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
@@ -8,7 +8,7 @@ import { useDispatch } from "react-redux";
 
 const Navigation = () => {
   const dispatch = useDispatch();
-
+  const [isDropdown, setDropdown] = useState(false);
   return (
     <nav className="nav">
       <NavLink to="/leaderboard" className="nav__link">
@@ -20,9 +20,15 @@ const Navigation = () => {
       <NavLink to="/" className="nav__profile">
         <div className="nav_profile-image"></div>
       </NavLink>
-      <button className="dropdown" onClick={() => dispatch(logoutUser())}>
+      <button className="dropdown" onClick={() => setDropdown(!isDropdown)}>
         <span>Profile name</span>
         <FontAwesomeIcon icon={faCaretDown}></FontAwesomeIcon>
+        <div className={isDropdown ? "dropdown-visible" : "dropdown-invisible"}>
+          <NavLink to="/profile">My Profile</NavLink>
+          <NavLink onClick={() => dispatch(logoutUser())} to="/login">
+            Log out
+          </NavLink>
+        </div>
       </button>
     </nav>
   );
